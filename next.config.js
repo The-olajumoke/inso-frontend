cabae11cdcef34e0a25b3edf6cc5ab0e3ab70080
Goodluck olajumoke <olajumokegoodluck@gmail.com>
2022-08-17 22:45:28 -0400

    fixing routing on staging

--- next.config.js
+++ next.config.js
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // reactStrictMode: true,
+  trailingSlash: true,
   images: {
     loader: "akamai",
     path: "",
   },
 };
 
-// module.exports = nextConfig;
-module.exports = {
-  trailingSlash: true,
-};
+module.exports = nextConfig;
