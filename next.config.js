33c862b832a36479ea4bab185ca2b28e86fcd9dc
Goodluck olajumoke <olajumokegoodluck@gmail.com>
2022-08-20 15:00:57 -0400

    setting up charts for each discussion

--- next.config.js
+++ next.config.js
@@ -1,15 +1,19 @@
-const withPWA = require("next-pwa");
+// const withPWA = require("next-pwa");
+// // const withOptimizedImages = require("next-optimized-images");
+// module.exports = withPWA({
+//   pwa: {
+//     dest: "public",
+//     register: true,
+//     skipWaiting: true,
+//   },
+//   images: {
+//     domains: [
+//       "res.cloudinary.com",
+//       "inso-staging.s3-website-us-east-1.amazonaws.com",
+//     ],
+//   },
+// });
+const withPlugins = require("next-compose-plugins");
+const optimizedImages = require("next-optimized-images");
 
-module.exports = withPWA({
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-  },
-  images: {
-    domains: [
-      "res.cloudinary.com",
-      "inso-staging.s3-website-us-east-1.amazonaws.com",
-    ],
-  },
-});
+module.exports = withPlugins([optimizedImages]);
